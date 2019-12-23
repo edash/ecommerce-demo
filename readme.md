680d34b6f9bb0f51d47f8e7290372d2a1aee5434
Elle Dashfield <eleanor.dashfield@ackama.com>
2019-12-24 08:48:30 +1300

    Initial setup of project

---
+++ readme.md
@@ -0,0 +1,1 @@
+This is a demo project showing the use of Capybara and RSpec to test a third-party ecommerce website.
---
+++ spec/spec_helper.rb
@@ -0,0 +1,23 @@
+require 'bundler'
+require 'capybara/dsl'
+require 'capybara/rspec'
+
+Bundler.setup(:default)
+Bundler.require
+
+# Which default browser do you want Selenium WebDriver to use?
+# :selenium_chrome # Selenium driving Chrome
+# :selenium_chrome_headless # Selenium driving Chrome in a headless configuration
+# https://github.com/teamcapybara/capybara#selenium
+
+Capybara.default_driver = :selenium_chrome
+Capybara.app_host = 'https://the-internet.herokuapp.com'
+Capybara.default_max_wait_time = 5
+
+# Run using:
+# bundle exec rspec spec/features/login.rb --format documentation
+# ... Or...
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
