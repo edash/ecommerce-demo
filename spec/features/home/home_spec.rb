73eb33cb380e816f4c719621dab009ec820f59b9
Elle Dashfield <eleanor.dashfield@ackama.com>
2019-12-25 08:57:21 +1300

    Created folders for home, cart and purchase. Started on home page.

---
+++ spec/features/home/home_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+feature "Home page" do
+  describe "Home page" do
+    context "When user is logged out" do
+      before do
+        visit root_path
+      end
+
+      it "can see home contents" do
+        expect(page).to have_content "BEST SELLERS"
+      end
+
+
+      context "When user is logged out" do
+      end
+    end
+  end
+end
