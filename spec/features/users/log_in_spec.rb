5a9d4f88940b81f20567ab6b971447717d37c93e
Elle Dashfield <eleanor.dashfield@ackama.com>
2019-12-26 17:09:41 +1300

    Added tests for unhappy login paths

--- spec/features/users/log_in_spec.rb
+++ spec/features/users/log_in_spec.rb
@@ -21,18 +21,43 @@ feature 'Log in' do
     expect(page).to have_css(".logout")
   end
 
-  # TODO
-  context "can't log in with invalid credentials" do
-   it "can't log in with blank fields"
-   it "can't log in with unregistered email address"
-   it "can't log in with wrong password"
-  end
-  
-    # TODO 
-    context "when user is logged out" do
-      xit "can see log in option" do
-        # .login "Sign in"
-      end
+  context "can't log in with missing or invalid credentials" do
+    it "can't log in with no email address" do
+      click_button "Sign in"
+      expect(page).to have_content "An email address required."
     end
 
+    it "can't log in with unregistered email address" do
+      within('#login_form') do
+        fill_in 'email', with: "notregistered@foo.com"
+        fill_in 'passwd', with: "password"
+        click_button "Sign in"
+      end
+      expect(page).to have_content "Authentication failed."
+    end
+
+    it "can't log in with correct email but no password" do
+        within('#login_form') do
+          fill_in 'email', with: "edashfield+test@gmail.com"
+          click_button "Sign in"
+        end
+        expect(page).to have_content "Password is required."
+      end
+
+    it "can't log in with wrong password" do
+      within('#login_form') do
+        fill_in 'email', with: "edashfield+test@gmail.com"
+        fill_in 'passwd', with: "wrongpassword"
+        click_button "Sign in"
+      end
+      expect(page).to have_content "Authentication failed."
+    end
+  end
+
+  context "when user is logged out" do
+    it "can see log in option" do
+      expect(page).to have_content "Sign in"
+      expect(page).to have_css ".login"
+    end
+  end
 end
