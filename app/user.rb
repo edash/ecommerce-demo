e752dedc55e3ef0cc064f25d9d839094086356c5
Elle Dashfield <eleanor.dashfield@ackama.com>
2019-12-27 07:31:38 +1300

    Added more attributes to users factory and formatted user.rb

--- app/user.rb
+++ app/user.rb
@@ -1,4 +1,4 @@
 class User
   attr_accessor :first_name, :last_name, :email, :password, :street_address,
-                :city, :state, :postal_code, :country, :phone
+                :city, :state, :postcode, :country, :phone
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,16 @@ require "faker"
 
 FactoryBot.define do
   factory :user, class: "User" do
-    first_name { Faker::Name.unique.first_name }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    # Ensure a unique email address is used for registration
     email { Faker::Internet.unique.email }
+    street_address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    state { "California" }
+    postcode { Faker::Address.postcode }
+    country { "United States" }
+    phone { Faker::PhoneNumber.cell_phone }
+    password { "password1" }
   end
 end
