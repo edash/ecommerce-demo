95cccf2a75cd747cd62259fb710af03c564dc9dc
Elle Dashfield <eleanor.dashfield@ackama.com>
2019-12-30 10:14:52 +1300

    WIP for go to checkout

--- spec/features/cart/add_item_to_cart_spec.rb
+++ spec/features/cart/add_item_to_cart_spec.rb
@@ -13,4 +13,31 @@ feature "Add item to cart" do
       expect(page).to have_content "Proceed to checkout"
     end
   end
+
+  xit "can go to checkout" do
+    add_default_item()
+    #expect(page).to have_content "Product successfully added to your shopping cart"
+    within("#layer_cart") do
+      #find(".button-medium") #works
+      #expect(page).to have_css(".button-medium") #works
+      click_button("Proceed to checkout") #fails, can't find nondisabled button
+      #binding.pry
+    end
+  end
+
+  # click_button ("Proceed to checkout", visible: :all) #fails
+  # expect(page).to have_content "SHOPPING-CART SUMMARY" #fails
+  #binding.pry
+
+  #works - can find success message but not go to checkout button :(
+  it "can go to checkout" do
+    add_default_item
+    expect(page).to have_content "Product successfully added to your shopping cart"
+  end
+end
+
+#TODO - decide whether best to have visit link in before block or here
+def add_default_item
+  visit "/index.php?id_product=1&controller=product"
+  click_button "Add to cart"
 end
