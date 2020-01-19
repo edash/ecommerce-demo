9ada5beaa2136428a153c52b2c6e942608f23598
Elle Dashfield <eleanor.dashfield@ackama.com>
2020-01-19 15:35:01 +1300

    Updated sign up spec explanantions and reduced comments

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,12 +4,12 @@ FactoryBot.define do
   factory :user, class: "User" do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    # Ensure a unique email address is used for registration
-    email { Faker::Internet.unique.email } #TODO find safe email
+    # Use example.com address
+    email { Faker::Internet.unique.safe_email }
     street_address { Faker::Address.street_address }
     city { Faker::Address.city }
     state { Faker::Address.state }
-    # Faker's zip code method returns an integer of varying length so it's not suitable
+    # Faker's zip code method returns an integer of varying length so not suitable
     postcode { Faker::Number.number(digits: 5) }
     country { "United States" }
     phone { Faker::PhoneNumber.cell_phone }
